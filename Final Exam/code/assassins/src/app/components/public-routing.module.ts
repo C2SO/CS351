eb650897477070083f98f42d17bc6031201dc2dc
TBSpeed <38928601+TBSpeed@users.noreply.github.com>
2019-05-28 13:51:27 -0400

    Sucessfull login page broken
    Updated graphics on login and regester pages
    made homepage avalible to all
    added button on home page to login
    created sucessfull login page
         -page does not work, all pages go blank when the sucessfull login page is put in the route regester

--- Final Exam/code/assassins/src/app/components/public-routing.module.ts
+++ Final Exam/code/assassins/src/app/components/public-routing.module.ts
@@ -4,15 +4,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { SucessComponent } from './sucess/sucess.component';
 import { AuthGuard } from '../core/guard/auth.guard';
 import { UserResolver } from '../core/resolver/user.resolver';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home-page', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent },
-  { path: 'home-page', component: HomePageComponent,  resolve: { data: UserResolver}}
+  //{ path: 'sucess', component: SucessComponent , resolve: { data: UserResolver}},
+  { path: 'home-page', component: HomePageComponent}
 ];
+// add 'resolve: { data: UserResolver}' to a route to make that page reguire a user to be logged in
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
